d226afe510981632b04e746da9d1a72bb5fc8d6c
milenasryan <147680950+milenasryan@users.noreply.github.com>
2023-12-06 11:14:39 -0800

    Create lcm.cpp

---
+++ lcm.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int gcd(int a, int b) 
+{
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+int lcm(int a, int b) 
+{
+    return (a * b) / gcd(a, b);
+}
+
+int main() 
+{
+    int num1, num2;
+
+    std::cout << "Enter two numbers: ";
+    std::cin >> num1 >> num2;
+
+    int result = lcm(num1, num2);
+
+    std::cout << "LCM of " << num1 << " and " << num2 << " is " << result << std::endl;
+
+    return 0;
+}
